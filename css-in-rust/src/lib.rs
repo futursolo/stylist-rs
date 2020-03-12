a23d91b726d1dbb63bab0da9fe5203ea702ab7e0
Lukas Wagner <appdev.lukaswagner@gmail.com>
2020-03-12 21:47:24 +0100

    v0.0.1

--- css-in-rust/src/lib.rs
+++ css-in-rust/src/lib.rs
@@ -6,6 +6,9 @@ extern crate wasm_bindgen;
 #[cfg(test)]
 extern crate wasm_bindgen_test;
 
+#[cfg(target_arch = "wasm32")]
 pub mod bindings;
+#[cfg(target_arch = "wasm32")]
 mod parser;
+#[cfg(target_arch = "wasm32")]
 pub mod style;
