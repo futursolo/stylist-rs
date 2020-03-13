43f6c07ad16d676c8f4e7935a7014177b7a72a55
Lukas Wagner <appdev.lukaswagner@gmail.com>
2020-03-13 22:08:00 +0100

    Correcting package.metadata.docs.rs to not include std_web

--- css-in-rust/build.rs
+++ css-in-rust/build.rs
@@ -5,10 +5,13 @@ use std::env;
 pub fn main() {
     let using_web_sys = cfg!(feature = "web_sys");
     let using_std_web = cfg!(feature = "std_web");
-    if using_web_sys && using_std_web {
-        panic!("CSSinRust does not allow the `web_sys` and `std_web` cargo features to be used simultaneously");
-    } else if !using_web_sys && !using_std_web {
-        panic!("CSSinRust requires selecting either the `web_sys` or `std_web` cargo feature");
+    let docs = cfg!(docs);
+    if !docs {
+        if using_web_sys && using_std_web {
+            panic!("CSSinRust does not allow the `web_sys` and `std_web` cargo features to be used simultaneously");
+        } else if !using_web_sys && !using_std_web {
+            panic!("CSSinRust requires selecting either the `web_sys` or `std_web` cargo feature");
+        }
     }
 
     let using_cargo_web = env::var("COMPILING_UNDER_CARGO_WEB").is_ok();
