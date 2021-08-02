7eb33419b04a1ea8c308b90b037b7a5140d29e2f
Kaede Hoshikawa <futursolo@icloud.com>
2021-08-02 22:15:04 +0900

    Make class name owned for Yew.

--- src/bindings/yew.rs
+++ src/bindings/yew.rs
@@ -10,7 +10,7 @@ use yew::html::Classes;
 impl From<Style> for Classes {
     fn from(style: Style) -> Self {
         let mut classes = Self::new();
-        classes.push(style.get_class_name());
+        classes.push(style.get_class_name().to_string());
         classes
     }
 }
