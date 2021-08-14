b4e429223677612c3dcf411002afc575d764660b
Kaede Hoshikawa <futursolo@icloud.com>
2021-08-14 19:44:00 +0900

    Separate AST for better readability.

---
+++ packages/stylist-core/src/ast/block.rs
@@ -0,0 +1,43 @@
+use std::fmt;
+
+use super::{StyleAttribute, ToStyleStr};
+
+/// A block is a set of css properties that apply to elements that
+/// match the condition. The CSS standard calls these "Qualified rules".
+///
+/// E.g.:
+/// ```css
+/// .inner {
+///     color: red;
+/// }
+/// ```
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Block {
+    /// If set to [`None`], signals to substitute with the classname generated for the
+    /// [`Sheet`] in which this is conatined. Otherwise substitute the classname for
+    /// each occuring '&', i.e. `None` is equivalent to `Some("&")`.
+    pub condition: Option<String>,
+    pub style_attributes: Vec<StyleAttribute>,
+}
+
+impl ToStyleStr for Block {
+    fn write_style<W: fmt::Write>(&self, w: &mut W, class_name: &str) -> fmt::Result {
+        if let Some(ref condition) = self.condition {
+            if condition.contains('&') {
+                let scoped_class = format!(".{}", class_name);
+                writeln!(w, "{} {{", condition.replace("&", scoped_class.as_str()))?;
+            } else {
+                writeln!(w, ".{} {} {{", class_name, condition)?;
+            }
+        } else {
+            writeln!(w, ".{} {{", class_name)?;
+        }
+
+        for attr in self.style_attributes.iter() {
+            attr.write_style(w, class_name)?;
+            writeln!(w)?;
+        }
+
+        write!(w, "}}")
+    }
+}
---
+++ packages/stylist-core/src/ast/rule.rs
@@ -0,0 +1,37 @@
+use std::fmt;
+
+use super::{RuleContent, ToStyleStr};
+
+/// An At-Rule can contain both other blocks and in some cases more At-Rules.
+///
+/// E.g.:
+/// ```css
+///  @keyframes move {
+///     from {
+///         width: 100px;
+///     }
+///     to {
+///         width: 200px;
+///     }
+/// }
+/// ```
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Rule {
+    pub condition: String,
+    /// Note that not all At-Rules allow arbitrary other At-Rules to appear
+    /// inside them, or arbitrary blocks. No safeguards at this point!
+    pub content: Vec<RuleContent>,
+}
+
+impl ToStyleStr for Rule {
+    fn write_style<W: fmt::Write>(&self, w: &mut W, class_name: &str) -> fmt::Result {
+        writeln!(w, "{} {{", self.condition)?;
+
+        for i in self.content.iter() {
+            i.write_style(w, class_name)?;
+            writeln!(w)?;
+        }
+
+        write!(w, "}}")
+    }
+}
