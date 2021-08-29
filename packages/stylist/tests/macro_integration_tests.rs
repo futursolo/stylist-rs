a3bd39bdbc9192ee98ba952a0da5b29038fa2dd8
Martin Molzer <ga65guy@mytum.de>
2021-08-29 20:15:23 +0200

    fixup

--- packages/stylist/tests/macro_integration_tests.rs
+++ packages/stylist/tests/macro_integration_tests.rs
@@ -4,10 +4,4 @@ fn test_macro_integrations() {
 
     t.pass("tests/macro_integrations/*-pass.rs");
     t.compile_fail("tests/macro_integrations/*-fail.rs");
-
-    let _ = stylist::style! {
-        .inner {
-            background-color: ${"4e4e4e"};
-        }
-    };
 }
