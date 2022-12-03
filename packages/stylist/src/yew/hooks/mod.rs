bb182dae46c7868a85ca94a3d45bf1a681b0bcf1
Kaede Hoshikawa <futursolo@icloud.com>
2022-12-04 02:29:44 +0900

    Add prepared media query hook.

--- packages/stylist/src/yew/hooks/mod.rs
+++ packages/stylist/src/yew/hooks/mod.rs
@@ -5,7 +5,7 @@ mod use_media_query;
 mod use_style;
 
 #[cfg(feature = "yew_use_media_query")]
-pub use use_media_query::use_media_query;
+pub use use_media_query::{use_media_query, use_prepared_media_query};
 
 #[cfg(feature = "yew_use_style")]
 pub use use_style::use_style;
