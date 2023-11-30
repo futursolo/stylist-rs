eb4a8e7aba2a45c5a4c1503ae25db66a355e2be9
伊欧 <langyo.china@gmail.com>
2023-11-30 21:09:39 +0800

    Use target_os instead of feature.

--- packages/stylist/src/arch.rs
+++ packages/stylist/src/arch.rs
@@ -1,19 +1,19 @@
 use crate::{Error, Result};
 pub use wasm_bindgen::JsValue;
 use web_sys::Window;
-#[cfg(all(target_arch = "wasm32", not(feature = "wasi")))]
+#[cfg(all(target_arch = "wasm32", not(target_os = "wasi")))]
 use web_sys::{Document, HtmlHeadElement};
 
 pub(crate) fn window() -> Result<Window> {
     web_sys::window().ok_or(Error::Web(None))
 }
 
-#[cfg(all(target_arch = "wasm32", not(feature = "wasi")))]
+#[cfg(all(target_arch = "wasm32", not(target_os = "wasi")))]
 pub(crate) fn document() -> Result<Document> {
     window()?.document().ok_or(Error::Web(None))
 }
 
-#[cfg(all(target_arch = "wasm32", not(feature = "wasi")))]
+#[cfg(all(target_arch = "wasm32", not(target_os = "wasi")))]
 pub(crate) fn doc_head() -> Result<HtmlHeadElement> {
     document()?.head().ok_or(Error::Web(None))
 }
